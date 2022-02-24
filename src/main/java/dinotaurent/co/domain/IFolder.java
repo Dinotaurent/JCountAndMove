4c3bde513b462d0f700c628162321f6102c34b1a
Dinotaurent <95384765+Dinotaurent@users.noreply.github.com>
2022-02-23 21:20:18 -0500

    Servicio CountAndMove hecho en java, creacion de logs y primera logica de moviento terminados.

---
+++ src/main/java/dinotaurent/co/domain/IFolder.java
@@ -0,0 +1,14 @@
+package dinotaurent.co.domain;
+
+/**
+ *
+ * @author dandazme
+ */
+public interface IFolder {
+    
+//    public boolean accept(File arch);
+    public void contar();
+    
+    public void mover();
+    
+}
---
+++ src/main/java/dinotaurent/co/test/Test.java
@@ -0,0 +1,19 @@
+package dinotaurent.co.test;
+
+import dinotaurent.co.domain.*;
+import org.apache.log4j.*;
+
+/**
+ *
+ * @author dandazme
+ */
+public class Test {
+    private static Logger log = Logger.getLogger(Test.class);
+    
+    public static void main(String[] args) {
+        var  vFolder = new FolderImpl();
+        vFolder.contar();
+        vFolder.mover();
+    }
+    
+}

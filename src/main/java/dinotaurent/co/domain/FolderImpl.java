80afbc2c484c8519d16d19a9ce8dbced35f2632c
Dinotaurent <95384765+Dinotaurent@users.noreply.github.com>
2022-03-03 23:52:09 -0500

    Mejora colosal al codigo

--- src/main/java/dinotaurent/co/domain/FolderImpl.java
+++ src/main/java/dinotaurent/co/domain/FolderImpl.java
@@ -9,6 +9,7 @@ import java.nio.file.attribute.FileTime;
 import java.text.SimpleDateFormat;
 import java.util.*;
 import java.nio.file.*;
+import java.util.stream.IntStream;
 
 /**
  *
@@ -26,9 +27,10 @@ public class FolderImpl implements IFolder {
     public File[] listadoTemp;
     public ArrayList<String> nombresArchivosEntrada = new ArrayList<String>();
     public ArrayList<String> nombresArchivosTemp = new ArrayList<String>();
-    public ArrayList<String> fechasArchivos = new ArrayList<String>();
     public ArrayList<String> dosificador = new ArrayList<String>();
-    public boolean movido = true;
+    public ArrayList<String> docNuevos = new ArrayList<String>();
+    public ArrayList<String> documentos = new ArrayList<String>();
+    public ArrayList<String> nombresDocNuevos = new ArrayList<String>();
     File pathEntrada = new File(PATH_ENTRADA);
     File pathTemp = new File(PATH_TEMP);
     BasicFileAttributes attrs;
@@ -36,41 +38,39 @@ public class FolderImpl implements IFolder {
     FileFilter filtro = (File file) -> !file.isHidden() && file.getName().endsWith(".txt");
 
     @Override
-
     public void contar() {
         listadoEntrada = pathEntrada.listFiles(filtro);
         listadoTemp = pathTemp.listFiles(filtro);
         contadorEntrada = listadoEntrada.length;
         contadorTemp = listadoTemp.length;
-//     System.out.println(contadorTemp);
-
+        SimpleDateFormat sdf = new SimpleDateFormat("MMddyyyyHHmmss");
         if (listadoEntrada != null || listadoEntrada.length != 0) {
-            SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+
             log.info("Se encontraron: " + contadorEntrada + " documentos en la ruta de entrada");
             for (File archivo : listadoEntrada) {
                 try {
                     attrs = Files.readAttributes(archivo.toPath(), BasicFileAttributes.class);
                     FileTime time = attrs.creationTime();
-                    nombresArchivosEntrada.add(archivo.getName());
-                    fechasArchivos.add(sdf.format(new Date(time.toMillis())));
-
+                    nombresArchivosEntrada.add(sdf.format(new Date(time.toMillis())) + archivo.getName());
+                    nombresDocNuevos.add(sdf.format(new Date(time.toMillis())) + archivo.getName());
                 } catch (IOException ex) {
                     log.error(ex);
                 }
             }
+            Collections.sort(nombresArchivosEntrada);
         }
 
         if (listadoTemp != null || listadoTemp.length != 0) {
             for (File archivo : listadoTemp) {
                 try {
                     attrs = Files.readAttributes(archivo.toPath(), BasicFileAttributes.class);
-                    nombresArchivosTemp.add(archivo.getName());
-
-//                    System.out.println(nombresArchivosTemp);
+                    FileTime time = attrs.creationTime();
+                    nombresArchivosTemp.add(sdf.format(new Date(time.toMillis())) + archivo.getName());
                 } catch (IOException ex) {
                     log.error(ex);
                 }
             }
+            Collections.sort(nombresArchivosTemp);
 
             if (!nombresArchivosTemp.isEmpty()) {
                 if (nombresArchivosTemp.size() < 5) {
@@ -87,23 +87,38 @@ public class FolderImpl implements IFolder {
         mover();
     }
 
+    @Override
     public void mover() {
 
         if (contadorEntrada > 5 && contadorTemp == 0) {
             System.out.println("Entro en la secuencia: A");
             log.info("Se ha detectado bloqueo, procedera a mover los documentos y reiniciar los servicios");
-
-            for (var archivo : nombresArchivosEntrada) {
-                Path documento = Path.of(PATH_ENTRADA + "\\" + archivo);
-                Path destino = Path.of(PATH_TEMP + archivo);
-//                System.out.println(documento);
-                try {
-                    Path mover = Files.move(documento, destino);
-                    log.info("Se movio el archivo " + documento + " a la ruta: " + PATH_TEMP);
-                } catch (Exception ex) {
-                    log.error(ex);
+            try {
+                //Se recorren los nombres y se elimina la fecha del nombre.
+                for (String archivo : nombresArchivosEntrada) {
+                    StringBuilder sb = new StringBuilder(archivo);
+                    for (int i = 0; i < 14; i++) {
+                        sb.deleteCharAt(0);
+                    }
+                    documentos.add(String.valueOf(sb));
                 }
+                //Se recorren los documentos y se crea un path con los nombres para ser movidos.
+                for (var archivo : documentos) {
+                    Path documento = Path.of(PATH_ENTRADA + "\\" + archivo);
+                    Path destino = Path.of(PATH_TEMP + archivo);
+//                System.out.println(documento);
+                    try {
+                        Path mover = Files.move(documento, destino);
+                        log.info("Se movio el archivo " + documento + " a la ruta: " + PATH_TEMP);
+                    } catch (Exception ex) {
+                        log.error(ex);
+                    }
+                }
+            } catch (Exception ex) {
+                log.error(ex);
             }
+
+            //Se reinician los servicios.
             try {
                 Thread.sleep(3000);
                 String[] cmd = {"sc.exe stop ServicioTestJavaX", "sc.exe config \"ServicioTestJavaX\" obj= \".\\usuario2\" password= \",,41qw96\"", "sc.exe start ServicioTestJavaX"};
@@ -117,29 +132,51 @@ public class FolderImpl implements IFolder {
             } catch (IOException | InterruptedException ex) {
                 log.error(ex);
             }
+
+            //Se limpian todos los arreglos para evitar sobreescritura.
+            documentos.clear();
+            docNuevos.clear();
+            dosificador.clear();
+            nombresArchivosTemp.clear();
+            nombresArchivosEntrada.clear();
+            nombresDocNuevos.clear();
             contar();
         } else if (contadorEntrada == 0 && contadorTemp > 0) {
             System.out.println("Entro en la secuencia: B");
             log.info("Se procedera a dosificar los documentos a la ruta de entrada");
-//
-//            System.out.println(dosificador.size());
-//            System.out.println(dosificador);
-            for (var archivo : dosificador) {
-                Path documento = Path.of(PATH_TEMP + archivo);
-                Path destino = Path.of(PATH_ENTRADA + "\\" + archivo);
-
-                try {
-                    Path mover = Files.move(documento, destino);
-                    log.info("Se movio el archivo " + documento + " a la ruta: " + PATH_ENTRADA);
-                    movido = true;
-                } catch (Exception ex) {
-                    log.error(ex);
+            try {
+                //Se recorren los nombres y se elimina la fecha del nombre.
+                for (String archivo : dosificador) {
+                    StringBuilder sb = new StringBuilder(archivo);
+                    for (int i = 0; i < 14; i++) {
+                        sb.deleteCharAt(0);
+                    }
+                    documentos.add(String.valueOf(sb));
                 }
+                //Se recorren los documentos y se crea un path con los nombres para ser movidos.
+                for (var archivo : documentos) {
+                    Path documento = Path.of(PATH_TEMP + archivo);
+                    Path destino = Path.of(PATH_ENTRADA + "\\" + archivo);
+
+                    try {
+                        Path mover = Files.move(documento, destino);
+                        log.info("Se movio el archivo " + documento + " a la ruta: " + PATH_ENTRADA);
+                    } catch (Exception ex) {
+                        log.error(ex);
+                    }
+                }
+
+            } catch (Exception ex) {
+                log.error(ex);
             }
 
+            //Se limpian todos los arreglos para evitar sobreescritura.
+            documentos.clear();
+            docNuevos.clear();
             dosificador.clear();
             nombresArchivosTemp.clear();
-
+            nombresArchivosEntrada.clear();
+            nombresDocNuevos.clear();
             contar();
 
         } else if (contadorEntrada == 0 && contadorTemp == 0 || contadorEntrada <= 5 && contadorTemp == 0) {
@@ -150,18 +187,75 @@ public class FolderImpl implements IFolder {
             } catch (InterruptedException ex) {
                 log.error(ex);
             }
+
+            //Se limpian todos los arreglos para evitar sobreescritura.
+            documentos.clear();
+            docNuevos.clear();
+            dosificador.clear();
+            nombresArchivosTemp.clear();
+            nombresArchivosEntrada.clear();
+            nombresDocNuevos.clear();
             contar();
         } else if (contadorEntrada <= 5 && contadorTemp > 0) {
-            System.out.println("Entre en la secuencia: E");
+            System.out.println("Entre en la secuencia: D");
             log.info("Aun no se han evacuado los documentos dosificados, se volvera a ejecutar dentro de 5 segundos");
             try {
                 Thread.sleep(10000);
             } catch (InterruptedException ex) {
                 log.error(ex);
             }
+
+            //Se limpian todos los arreglos para evitar sobreescritura.
+            documentos.clear();
+            docNuevos.clear();
+            dosificador.clear();
+            nombresArchivosTemp.clear();
+            nombresArchivosEntrada.clear();
+            nombresDocNuevos.clear();
+            contar();
+        } else if (contadorEntrada > 5 && contadorTemp > 0) {
+            System.out.println("Entre en la secuencia: E");
+            log.info("Se encontraron nuevos documentos que bloquearian en servicio, se procedera a mover a la ruta temporal");
+            //Se ordena los nombres de todos los docs de la ruta de entrada.
+            Collections.sort(nombresDocNuevos);
+            try {
+                //Se agregan a docNuevos los nombres de los documentos nuevos.
+                for (int i = 5; i < contadorEntrada; i++) {
+                    docNuevos.add(nombresDocNuevos.get(i));
+                }
+                //Se recorren los nombres y se elimina la fecha del nombre.
+                for (String archivo : docNuevos) {
+                    StringBuilder sb = new StringBuilder(archivo);
+                    for (int i = 0; i < 14; i++) {
+                        sb.deleteCharAt(0);
+                    }
+                    documentos.add(String.valueOf(sb));
+                }
+                //Se recorren los documentos y se crea un path con los nombres para ser movidos.
+                for (var archivo : documentos) {
+                    Path documento = Path.of(PATH_ENTRADA + "\\" + archivo);
+                    Path destino = Path.of(PATH_TEMP + archivo);
+                    try {
+                        Path mover = Files.move(documento, destino);
+                        log.info("Se movio el archivo " + documento + " a la ruta: " + PATH_TEMP);
+                    } catch (Exception ex) {
+                        log.error(ex);
+                    }
+                }
+                Thread.sleep(3000);
+            } catch (Exception ex) {
+                log.error(ex);
+            }
+
+            //Se limpian todos los arreglos para evitar sobreescritura.
+            documentos.clear();
+            docNuevos.clear();
+            dosificador.clear();
+            nombresArchivosTemp.clear();
+            nombresArchivosEntrada.clear();
+            nombresDocNuevos.clear();
             contar();
         }
 
     }
-
 }
